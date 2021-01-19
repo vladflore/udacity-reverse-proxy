99770eee82cddf3d1af1925760aae10e0a2fa90c
Vlad Flore <flore.vlad@gmail.com>
2021-01-19 19:43:23 +0100

    Remove travis yml files and dependencies

--- build-app-image.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Building App Docker Image"
-cd ./app
-docker --version
-# docker build -t simple-express .
-# docker tag simple-express vladflore/simple-express:latest
--- build-reverse-proxy-image.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Building Reverse Proxy Docker Image"
-cd ./reverse-proxy
-docker --version
-# docker build -t simple-reverse-proxy .
-# docker tag simple-reverse-proxy vladflore/simple-reverse-proxy:latest
