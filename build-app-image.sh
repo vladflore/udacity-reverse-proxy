cfbb96c74562d15b46f0900d268801e13cc804dd
Vlad Flore <flore.vlad@gmail.com>
2021-01-19 19:08:55 +0100

    Update travis yml

--- build-app-image.sh
+++ build-app-image.sh
@@ -2,5 +2,5 @@
 echo "Building App Docker Image"
 cd ./app
 docker --version
-docker build -t simple-express .
-docker tag simple-express vladflore/simple-express:latest
+# docker build -t simple-express .
+# docker tag simple-express vladflore/simple-express:latest
--- build-reverse-proxy-image.sh
+++ build-reverse-proxy-image.sh
@@ -2,5 +2,5 @@
 echo "Building Reverse Proxy Docker Image"
 cd ./reverse-proxy
 docker --version
-docker build -t simple-reverse-proxy .
-docker tag simple-reverse-proxy vladflore/simple-reverse-proxy:latest
+# docker build -t simple-reverse-proxy .
+# docker tag simple-reverse-proxy vladflore/simple-reverse-proxy:latest
